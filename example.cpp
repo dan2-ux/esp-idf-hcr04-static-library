1b18da61f4f6d9e83841f5d01c1b925549f9e10b
Pham Cong Danh <danhlanhat9@gmail.com>
2026-01-05 20:58:43 +0700

    Refactor distanceDisplay and remove unused timer
    Refactor distanceDisplay function to remove timer and memory allocation.

--- example.cpp
+++ example.cpp
@@ -2,39 +2,21 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-TimerHandle_t timer1;
-
-void timerFunction(TimerHandle_t xTimer)
-{
-    gpio_set_level(GPIO_NUM_2, 0);
-}
-
 void distanceDisplay(void *arg)
 {
     distance::gpioINFO *gpio = (distance::gpioINFO *)arg;
-    float dis = 0;
     while (1)
     {
-        char *distanceText = (char *)malloc(30);
-        dis = measure(gpio);
-        if (dis == -1)
+        if (distance::measure(gpio))
         {
-            return;
+            displayAndWarning(gpio);
         }
-        snprintf(distanceText, 20, "Distance: %.2f cm", dis);
-        ESP_LOGI(TAG, "%s", distanceText);
-        if (dis < 10)
+        else
         {
-            gpio_set_level(GPIO_NUM_2, 1);
+            ESP_LOGE(TAG, "Error can't measure distance");
+        }
 
-            if (xTimerIsTimerActive(timer1) != false)
-            {
-                xTimerStop(timer1, portMAX_DELAY);
-            }
-            xTimerStart(timer1, portMAX_DELAY);
-        }
         vTaskDelay(pdMS_TO_TICKS(100));
-        free(distanceText);
     }
 }
 
@@ -43,14 +25,10 @@ extern "C" void app_main()
     static distance::gpioINFO gpio = {
         .trig = GPIO_NUM_4,
         .echo = GPIO_NUM_5,
+        .warning = GPIO_NUM_2,
     };
 
     distance::gpioINIT(&gpio);
-
-    gpio_reset_pin(GPIO_NUM_2);
-    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
-
-    timer1 = xTimerCreate("turn led on when distance < 10", pdMS_TO_TICKS(1000), pdTRUE, (void *)0, timerFunction);
 
     xTaskCreatePinnedToCore(distanceDisplay, "displaying distance", 2048, &gpio, 5, NULL, 1);
 }
