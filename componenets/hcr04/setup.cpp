9bd333033987ec76bd757b1002de1e40cc4a4017
Pham Cong Danh <danhlanhat9@gmail.com>
2026-01-05 20:58:08 +0700

    Update setup.cpp

--- componenets/hcr04/setup.cpp
+++ componenets/hcr04/setup.cpp
@@ -1,9 +1,20 @@
 #include "setup.h"
 #include "esp_timer.h"
 #include "esp_rom_sys.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+TimerHandle_t timer1 = NULL;
+QueueHandle_t q1 = NULL;
 
 namespace distance
 {
+    void warningTask(TimerHandle_t xTimer)
+    {
+        gpioINFO *gpio = (gpioINFO *)pvTimerGetTimerID(xTimer);
+        gpio_set_level(gpio->warning, 0);
+    }
+
     void gpioINIT(gpioINFO *gpio)
     {
         if (gpio == nullptr)
@@ -12,12 +23,16 @@ namespace distance
         }
         gpio_reset_pin(gpio->trig);
         gpio_reset_pin(gpio->echo);
+        gpio_reset_pin(gpio->warning);
 
         gpio_set_direction(gpio->echo, GPIO_MODE_INPUT);
         gpio_set_direction(gpio->trig, GPIO_MODE_OUTPUT);
+        gpio_set_direction(gpio->warning, GPIO_MODE_OUTPUT);
+        q1 = xQueueCreate(5, sizeof(float));
+        timer1 = xTimerCreate("timer for warning", pdMS_TO_TICKS(500), pdTRUE, (void *)gpio, warningTask);
     }
 
-    float measure(gpioINFO *gpio)
+    bool measure(gpioINFO *gpio)
     {
         if (gpio == nullptr)
         {
@@ -51,6 +66,36 @@ namespace distance
         }
 
         int64_t echoEnd = esp_timer_get_time();
-        return ((echoEnd - echoStart) * 0.0343) / 2;
+        float res = ((echoEnd - echoStart) * 0.0343) / 2;
+
+        if (xQueueSend(q1, &res, 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void displayAndWarning(gpioINFO *gpio)
+    {
+        float res = 0;
+        char *text = (char *)malloc(30);
+        if (xQueueReceive(q1, &res, 0) == pdTRUE)
+        {
+            if (res < 10)
+            {
+                gpio_set_level(gpio->warning, 1);
+                if (xTimerIsTimerActive(timer1) == pdTRUE)
+                {
+                    xTimerReset(timer1, 0);
+                }
+                xTimerStart(timer1, 0);
+            }
+            snprintf(text, 30, "Distance: %.2f cm", res);
+            ESP_LOGI(TAG, "%s", text);
+        }
+        free(text);
     }
 }
